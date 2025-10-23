8ee0d2d7f5331bb17275f6fee062b5699f811f64
jsw4335 <jsw4335@gmail.com>
2025-10-23 22:40:52 +0900

    장바구니기능 구현

--- routes/carts.js
+++ routes/carts.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
+const {
+    addToCart,
+    getCartItems,
+    removeCartItem,
+} = require("../controller/CartController");
 router.use(express.json());
 
-router
-    .route("/")
-    .post((req, res) => {
-        res.json("장바구니 담기");
-    })
-    .get((req, res) => {
-        res.json("장바구니 조회");
-    });
-router.delete("/:id", (req, res) => {
-    res.json("장바구니 도서삭제");
-});
-//장바구니에서 선택한 주문 예상 상품 목록조회는 어떻게 할거?
-//url 어떻게 할지 고민중
-// router.get("/:", (req, res) => {
-//     res.json("주문 예상 상품 조회");
-// });
+router.route("/").post(addToCart).get(getCartItems); //장바구니에서 선택한 주문 예상 상품 목록조회(get에서 같이함)
+router.delete("/:id", removeCartItem);
+
 module.exports = router;
